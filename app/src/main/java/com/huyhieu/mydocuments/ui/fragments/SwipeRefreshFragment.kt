083e6162e252906683bd14210b87b0fef581a703
Cu Huy Hieu <hieucu@wee.vn>
2022-10-07 11:26:37 +0700

    Add SwipeRefreshFragment.kt

--- app/src/main/java/com/huyhieu/mydocuments/ui/fragments/SwipeRefreshFragment.kt
+++ app/src/main/java/com/huyhieu/mydocuments/ui/fragments/SwipeRefreshFragment.kt
@@ -1,15 +1,65 @@
 package com.huyhieu.mydocuments.ui.fragments
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.view.View
+import androidx.lifecycle.lifecycleScope
+import com.huyhieu.mydocuments.R
 import com.huyhieu.mydocuments.base.BaseFragment
 import com.huyhieu.mydocuments.databinding.FragmentSwipeRefreshBinding
+import com.huyhieu.mydocuments.utils.extensions.onTransitionCompleted
+import com.huyhieu.mydocuments.utils.extensions.showToastShort
+import com.huyhieu.mydocuments.utils.logDebug
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class SwipeRefreshFragment : BaseFragment<FragmentSwipeRefreshBinding>() {
     override fun initializeBinding() = FragmentSwipeRefreshBinding.inflate(layoutInflater)
 
     override fun FragmentSwipeRefreshBinding.addControls(savedInstanceState: Bundle?) {
+        handleOnTouchView(nstView)
+        handleOnTouchView(rcv)
     }
 
     override fun FragmentSwipeRefreshBinding.addEvents(savedInstanceState: Bundle?) {
+        root.onTransitionCompleted { _, currentId ->
+            when (currentId) {
+                R.id.refresh -> {
+                    context.showToastShort("Loading...")
+                    lifecycleScope.launch {
+                        delay(1000)
+                        if (root.progress != 0F && root.currentState == R.id.refresh) {
+                            root.transitionToStart()
+                        }
+                        context.showToastShort("Done!")
+                    }
+                }
+            }
+
+        }
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    fun FragmentSwipeRefreshBinding.handleOnTouchView(view: View) {
+        var yView = 0F
+        view.setOnTouchListener { _, event ->
+            if (root.progress == 0f && root.startState == R.id.start) {
+                /*if (yView >= event.y && !usuperHistoryAdapter.dataIsEmpty) {
+                    root.setTransition(R.id.transAnchor)
+                } else {
+                    root.setTransition(R.id.transRefresh)
+                }
+                recyclerViewUsuperHistory.scrollToPosition(0)*/
+                if (yView >= event.y) {
+                    root.setTransition(R.id.transAnchor)
+                } else {
+                    root.setTransition(R.id.transRefresh)
+                }
+                rcv.scrollToPosition(0)
+                logDebug("$yView - ${event.y}")
+            }
+            yView = event.y
+            return@setOnTouchListener false
+        }
     }
 }
--- app/src/main/java/com/huyhieu/mydocuments/utils/extensions/MotionExtensions.kt
+++ app/src/main/java/com/huyhieu/mydocuments/utils/extensions/MotionExtensions.kt
@@ -10,3 +10,35 @@ fun MotionLayout.setTransitionTo(startID: Int, endID: Int, duration: Int = 500) 
         transitionToEnd()
     }
 }
+
+fun MotionLayout.onTransitionCompleted(onComplete: ((motionLayout: MotionLayout?, currentId: Int) -> Unit)? = null) {
+    addTransitionListener(object : MotionLayout.TransitionListener {
+        override fun onTransitionStarted(
+            motionLayout: MotionLayout?,
+            startId: Int,
+            endId: Int
+        ) {
+
+        }
+
+        override fun onTransitionChange(
+            motionLayout: MotionLayout?,
+            startId: Int,
+            endId: Int,
+            progress: Float
+        ) {
+        }
+
+        override fun onTransitionCompleted(motionLayout: MotionLayout?, currentId: Int) {
+            onComplete?.invoke(motionLayout, currentId)
+        }
+
+        override fun onTransitionTrigger(
+            motionLayout: MotionLayout?,
+            triggerId: Int,
+            positive: Boolean,
+            progress: Float
+        ) {
+        }
+    })
+}
