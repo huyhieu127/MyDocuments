e15102c070aad5fbfa43f4a3817333ab37395726
Cu Huy Hieu <hieucu@wee.vn>
2022-11-11 18:29:08 +0700

    add frg AnalogClockFragment.kt

---
+++ app/src/main/java/com/huyhieu/mydocuments/ui/fragments/components/AnalogClockFragment.kt
@@ -0,0 +1,17 @@
+package com.huyhieu.mydocuments.ui.fragments.components
+
+import android.os.Bundle
+import com.huyhieu.mydocuments.base.BaseFragment
+import com.huyhieu.mydocuments.databinding.FragmentAnalogClockBinding
+
+class AnalogClockFragment : BaseFragment<FragmentAnalogClockBinding>() {
+
+    override fun initializeBinding() = FragmentAnalogClockBinding.inflate(layoutInflater)
+
+    override fun FragmentAnalogClockBinding.addControls(savedInstanceState: Bundle?) {
+    }
+
+    override fun FragmentAnalogClockBinding.addEvents(savedInstanceState: Bundle?) {
+    }
+
+}
