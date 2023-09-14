9a4e73a54f0e30340d9c3c1872327c7591e682b3
huyhieu127 <id.huyhieu@gmail.com>
2023-09-14 12:35:29 +0700

    Upgrade performance load video using cache

--- app/src/main/java/com/huyhieu/mydocuments/ui/fragments/tiktok/TikTokFragment.kt
+++ app/src/main/java/com/huyhieu/mydocuments/ui/fragments/tiktok/TikTokFragment.kt
@@ -22,161 +22,58 @@ import com.huyhieu.mydocuments.models.TikTokVideoForm
 import com.huyhieu.mydocuments.utils.readAssets
 import com.huyhieu.mydocuments.utils.toData
 import com.huyhieu.mydocuments.utils.toJson
+import com.huyhieu.mydocuments.utils.tryCatch
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
+@OptIn(UnstableApi::class)
 @AndroidEntryPoint
 class TikTokFragment : BaseFragmentVM<FragmentTikTokBinding, TikTokVM>() {
 
-    private val forYouAdapter by lazy { TikTokPlayerAdapter() }
-
-    private val player by lazy {
-        ExoPlayer.Builder(requireContext()).build().apply {
-            playWhenReady = true
-            repeatMode = Player.REPEAT_MODE_ONE
-        }
-    }
+    private val forYouAdapter by lazy { TikTokPlayerAdapter(requireContext()) }
 
     private val vpCallback = object : ViewPager2.OnPageChangeCallback() {
-        override fun onPageScrolled(
-            position: Int,
-            positionOffset: Float,
-            positionOffsetPixels: Int
-        ) {
-            super.onPageScrolled(position, positionOffset, positionOffsetPixels)
-//            val y = (-positionOffsetPixels).toFloat()
-//            vb.playerView.y = y
-//            vb.lnContainer.y = y
-            val x = (-positionOffsetPixels).toFloat()
-            //logDebug("$positionOffsetPixels  --------- $position -> $positionOffset")
-            vb.playerView.x = x
-            vb.frameController.root.x = x
-        }
 
         override fun onPageSelected(position: Int) {
             super.onPageSelected(position)
-            player.pause()
             jobTime?.cancel()
 
+            val player = forYouAdapter.storedPlayers[position]
             val oldIndex = player.currentMediaItemIndex
-            val seekTime = forYouAdapter.listData[position].timePlayed
+            val seekTime = forYouAdapter.listVideo[position].timePlayed
 
             //Save time played of video played
             val timePlayed = player.currentPosition
-            forYouAdapter.listData[oldIndex].timePlayed = timePlayed
-
-            //Save last frame of video played
-            screenshotLatestFrame(oldIndex)
+            forYouAdapter.listVideo[oldIndex].timePlayed = timePlayed
 
             //Play video in this page
-            vb.playerView.isInvisible = true
             player.seekTo(position, seekTime)
             val duration = player.duration
             val time = (seekTime * 100) / duration
-            vb.frameController.seekBarDuration.progress = time.toInt()
+            tryCatch {
+                setupSeekBar(player)
+                vb.seekBarDuration.progress = time.toInt()
+            }
             player.play()
         }
     }
 
-    private val playerCallback = object : Player.Listener {
-        override fun onIsLoadingChanged(isLoading: Boolean) {
-            super.onIsLoadingChanged(isLoading)
-            val isShow = isLoading && !player.isPlaying
-            vb.prgLoading.isVisible = isShow
-            logDebug("onIsLoadingChanged $isShow")
-        }
-
-        //End video
-        override fun onPositionDiscontinuity(
-            oldPosition: Player.PositionInfo,
-            newPosition: Player.PositionInfo,
-            reason: Int
-        ) {
-            super.onPositionDiscontinuity(oldPosition, newPosition, reason)
-            if (reason == Player.DISCONTINUITY_REASON_AUTO_TRANSITION) {
-                lifecycleScope.launch {
-                    jobTime?.cancel()
-                    vm.setSeekToProgressBar(100)
-                    delay(1000)
-                    if (player.isPlaying) getCurrentPlayerPosition()
-                }
-            }
-        }
-
-        override fun onIsPlayingChanged(isPlaying: Boolean) {
-            super.onIsPlayingChanged(isPlaying)
-            if (isPlaying) {
-                vb.playerView.isInvisible = false
-                getCurrentPlayerPosition()
-            }
-            if (isPlaying) {
-                vb.prgLoading.isVisible = false
-            } else {
-                vb.prgLoading.isVisible = player.isLoading
-            }
-
-            logDebug("$isPlaying -- ${player.isLoading}")
-        }
-    }
-
-    @OptIn(UnstableApi::class)
-    private fun screenshotLatestFrame(index: Int) {
-//        tryCatch {
-//            val bitmap = (vb.playerView.videoSurfaceView as TextureView).bitmap
-//            forYouAdapter.listData[index].thumbnail = bitmap
-//            logDebug("screenshotLatestFrame - $index - ${bitmap != null}")
-//            if (forYouAdapter.listData[index].thumbnail != null) {
-//                forYouAdapter.notifyItemChanged(index)
-//            }
-//        }
-    }
-
     override fun onMyViewCreated(savedInstanceState: Bundle?) {
         mActivity.setNavigationBarColor(R.color.black)
-        initPlayerView()
         initViewForYou()
-        vb.apply {
-            setClickViews(frameController.imgWhiteHeart, frameController.imgLottieHeart)
-        }
-    }
-
-    override fun FragmentTikTokBinding.onClickViewBinding(v: View, id: Int) {
-        when (v) {
-            frameController.imgWhiteHeart -> {
-                with(frameController.imgLottieHeart) {
-                    isVisible = true
-                    scaleX = 1F
-                    scaleY = 1F
-                    playAnimation()
-                }
-            }
-
-            frameController.imgLottieHeart -> {
-                with(frameController.imgLottieHeart) {
-                    animate().scaleX(0F).scaleY(0F).withEndAction {
-                        isInvisible = true
-                    }
-                }
-            }
-        }
     }
 
     override fun onMyLiveData(savedInstanceState: Bundle?) {
         vm.currentPosition.observeNoneNull {
-            vb.frameController.seekBarDuration.setProgress(it.toInt(), true)
+            vb.seekBarDuration.setProgress(it.toInt(), true)
         }
     }
 
-    @OptIn(UnstableApi::class)
-    private fun initPlayerView() = with(vb) {
-        playerView.player = player
-        playerView.useController = false
-        player.addListener(playerCallback)
-
-        vb.frameController.seekBarDuration.setOnSeekBarChangeListener(object :
-            SeekBar.OnSeekBarChangeListener {
+    private fun setupSeekBar(player: Player) {
+        getCurrentPlayerPosition(player)
+        vb.seekBarDuration.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
             override fun onProgressChanged(v: SeekBar?, progress: Int, isFromUser: Boolean) {
                 if (isFromUser) {
                     val duration = player.duration
@@ -192,9 +89,8 @@ class TikTokFragment : BaseFragmentVM<FragmentTikTokBinding, TikTokVM>() {
 
             override fun onStopTrackingTouch(v: SeekBar?) {
                 player.play()
-                getCurrentPlayerPosition()
+                getCurrentPlayerPosition(player)
             }
-
         })
     }
 
@@ -203,6 +99,7 @@ class TikTokFragment : BaseFragmentVM<FragmentTikTokBinding, TikTokVM>() {
         loadForYou()
     }
 
+    @OptIn(UnstableApi::class)
     private fun loadForYou() {
         val json = readAssets("explore.json")
         val jsonObject = json.toData<JsonObject>()
@@ -213,21 +110,10 @@ class TikTokFragment : BaseFragmentVM<FragmentTikTokBinding, TikTokVM>() {
         listTikTokVideos.first().isPlaying = true
         forYouAdapter.fillData(listTikTokVideos)
         vb.vp2ForYou.registerOnPageChangeCallback(vpCallback)
-        vb.vp2ForYou.setOnTouchListener(View.OnTouchListener { v, motionEvent ->
-            return@OnTouchListener false
-        })
-
-        playVideo(listTikTokVideos)
-    }
-
-    private fun playVideo(videos: MutableList<TikTokVideoForm>) {
-        val mediaItems = videos.map { MediaItem.fromUri(it.url) }
-        player.addMediaItems(mediaItems)
-        player.prepare()
     }
 
     private var jobTime: Job? = null
-    private fun getCurrentPlayerPosition() {
+    private fun getCurrentPlayerPosition(player: Player) {
         jobTime?.cancel()
         jobTime = lifecycleScope.launch {
             val duration = player.duration
@@ -235,18 +121,35 @@ class TikTokFragment : BaseFragmentVM<FragmentTikTokBinding, TikTokVM>() {
             val time = (position * 100) / duration
             vm.setSeekToProgressBar(time)
             delay(1000)
-            if (player.isPlaying) getCurrentPlayerPosition()
+            if (player.isPlaying) getCurrentPlayerPosition(player)
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        tryCatch {
+            val currentIndex = vb.vp2ForYou.currentItem
+            getCurrentPlayerPosition(forYouAdapter.storedPlayers[currentIndex])
+            forYouAdapter.playVideo(currentIndex)
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        tryCatch {
+            jobTime?.cancel()
+            forYouAdapter.pauseVideo(vb.vp2ForYou.currentItem)
         }
     }
 
     override fun onDestroyView() {
         vb.vp2ForYou.unregisterOnPageChangeCallback(vpCallback)
-        player.removeListener(playerCallback)
         super.onDestroyView()
     }
 
     override fun onDestroy() {
         super.onDestroy()
+        forYouAdapter.releaseAllPlayer()
         logDebug("onDestroy")
     }
 }
--- app/src/main/java/com/huyhieu/mydocuments/ui/fragments/tiktok/TikTokPlayerAdapter.kt
+++ app/src/main/java/com/huyhieu/mydocuments/ui/fragments/tiktok/TikTokPlayerAdapter.kt
@@ -1,26 +1,244 @@
 package com.huyhieu.mydocuments.ui.fragments.tiktok
 
 import android.annotation.SuppressLint
+import android.content.Context
+import android.util.SparseArray
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.core.util.forEach
+import androidx.core.view.isInvisible
+import androidx.core.view.isVisible
+import androidx.media3.common.MediaItem
+import androidx.media3.common.Player
+import androidx.media3.common.util.UnstableApi
+import androidx.media3.database.StandaloneDatabaseProvider
+import androidx.media3.datasource.DefaultDataSource
+import androidx.media3.datasource.DefaultHttpDataSource
+import androidx.media3.datasource.cache.CacheDataSource
+import androidx.media3.datasource.cache.CacheKeyFactory
+import androidx.media3.datasource.cache.LeastRecentlyUsedCacheEvictor
+import androidx.media3.datasource.cache.SimpleCache
+import androidx.media3.exoplayer.DefaultLoadControl
+import androidx.media3.exoplayer.ExoPlayer
+import androidx.media3.exoplayer.source.DefaultMediaSourceFactory
+import androidx.media3.exoplayer.source.MediaSource
+import androidx.media3.exoplayer.source.ProgressiveMediaSource
+import androidx.media3.exoplayer.upstream.DefaultAllocator
+import androidx.media3.ui.AspectRatioFrameLayout
+import androidx.media3.ui.PlayerView
 import androidx.recyclerview.widget.RecyclerView
-import com.huyhieu.mydocuments.base.BaseSimpleAdapter
+import com.huyhieu.mydocuments.R
 import com.huyhieu.mydocuments.databinding.ItemTikTokPlayerBinding
+import com.huyhieu.mydocuments.libraries.extensions.color
 import com.huyhieu.mydocuments.models.TikTokVideoForm
-import com.huyhieu.mydocuments.utils.load
+import com.huyhieu.mydocuments.utils.delayed
+import com.huyhieu.mydocuments.utils.tryCatch
+import java.io.File
 
-class TikTokPlayerAdapter : BaseSimpleAdapter<ItemTikTokPlayerBinding, TikTokVideoForm>() {
-    @SuppressLint("CheckResult")
-    override fun ItemTikTokPlayerBinding.onBindMyViewHolder(
-        holder: RecyclerView.ViewHolder,
-        item: TikTokVideoForm,
-        position: Int
-    ) {
-        //imgThumbnail.alpha = if (item.isPlaying) 0F else 1F
-        if (item.thumbnail != null) {
-            imgThumbnail.load(item.thumbnail){
-                centerCrop()
-            }
-        } else {
-            imgThumbnail.load(item.url)
+@UnstableApi
+class TikTokPlayerAdapter(val context: Context) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    var listVideo = listOf<TikTokVideoForm>()
+
+    private val cacheSize: Long = 100 * 1024 * 1024 //100MB
+    private val leastRecentlyUsedCacheEvictor by lazy { LeastRecentlyUsedCacheEvictor(cacheSize) }
+    private val databaseProvider by lazy { StandaloneDatabaseProvider(context) }
+    private val simpleCache by lazy {
+        SimpleCache(
+            File(context.cacheDir, "tiktok_video"), leastRecentlyUsedCacheEvictor, databaseProvider
+        )
+    }
+    private val cacheFactory by lazy {
+        val httpDataSourceFactory =
+            DefaultHttpDataSource.Factory().setAllowCrossProtocolRedirects(true)
+
+        val defaultDataSourceFactory = DefaultDataSource.Factory(context, httpDataSourceFactory)
+
+        CacheDataSource.Factory().apply {
+            cacheKeyFactory = CacheKeyFactory.DEFAULT
+            setCache(simpleCache)
+            setUpstreamDataSourceFactory(defaultDataSourceFactory)
+            setFlags(CacheDataSource.FLAG_IGNORE_CACHE_ON_ERROR)
         }
+    }
+
+    var storedMedias: SparseArray<String> = SparseArray()
+    var storedPlayers: SparseArray<ExoPlayer> = SparseArray()
+    var storedPlayerView: SparseArray<PlayerView> = SparseArray()
+
+    @SuppressLint("NotifyDataSetChanged")
+    fun fillData(listVideo: List<TikTokVideoForm>) {
+        this.listVideo = listVideo
+        notifyDataSetChanged()
+    }
+
+    fun releaseAllPlayer() {
+        tryCatch {
+            this.storedPlayers.forEach { _, value ->
+                value.release()
+            }
+            simpleCache.release()
+        }
+    }
+
+    fun playVideo(position: Int) {
+        tryCatch {
+            storedPlayers[position].play()
+        }
+    }
+
+    fun pauseVideo(position: Int) {
+        tryCatch {
+            storedPlayers[position].pause()
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        val bind =
+            ItemTikTokPlayerBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return TikTokPlayerVH(bind)
+    }
+
+    override fun getItemCount(): Int = if (listVideo.isEmpty()) 3 else listVideo.size
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        if (listVideo.isNotEmpty() && holder is TikTokPlayerVH) {
+            holder.bindViewHolder()
+        }
+    }
+
+    override fun onViewAttachedToWindow(holder: RecyclerView.ViewHolder) {
+        super.onViewAttachedToWindow(holder)
+        if (listVideo.isNotEmpty() && holder is TikTokPlayerVH) {
+            with(holder.vb) {
+                val position = holder.layoutPosition
+
+                val playerView = storedPlayerView.get(position)
+                val player = storedPlayers.get(position)
+                val urlMediaItem = storedMedias.get(position)
+
+                //viewLoading.root.isVisible = true
+                framePreview.addView(playerView)
+                playerView.isInvisible = true
+
+                playerView.player = player
+                player.playVideo(urlMediaItem)
+
+            }
+        }
+    }
+
+    override fun onViewDetachedFromWindow(holder: RecyclerView.ViewHolder) {
+        super.onViewDetachedFromWindow(holder)
+        if (listVideo.isNotEmpty() && holder is TikTokPlayerVH) {
+            with(holder.vb) {
+                val position = holder.layoutPosition
+                val indexOfChild = framePreview.indexOfChild(storedPlayerView.get(position))
+                if (indexOfChild >= 0) {
+                    framePreview.removeViewAt(indexOfChild)
+                    storedPlayers.get(position).stop()
+                }
+            }
+        }
+    }
+
+    inner class TikTokPlayerVH(val vb: ItemTikTokPlayerBinding) : RecyclerView.ViewHolder(vb.root) {
+
+        private val playerCallback = object : Player.Listener {
+
+            override fun onIsLoadingChanged(isLoading: Boolean) {
+                super.onIsLoadingChanged(isLoading)
+                if (!isLoading) {
+                    delayed {
+                        //vb.viewLoading.root.isVisible = false
+                    }
+                }
+            }
+
+            override fun onIsPlayingChanged(isPlaying: Boolean) {
+                super.onIsPlayingChanged(isPlaying)
+                if (isPlaying) {
+                    tryCatch {
+                        storedPlayerView[layoutPosition].isVisible = true
+                        delayed {
+                            //vb.viewLoading.root.isVisible = false
+                        }
+                    }
+                }
+            }
+        }
+
+        fun bindViewHolder() {
+            val video = listVideo[layoutPosition]
+            storedMedias.append(layoutPosition, video.url)
+            storedPlayerView.append(layoutPosition, getVideoSource())
+            storedPlayers.append(layoutPosition, getPlayer(playerCallback))
+
+            with(vb.frameController) {
+                imgWhiteHeart.setOnClickListener {
+                    imgLottieHeart.apply {
+                        isVisible = true
+                        scaleX = 1F
+                        scaleY = 1F
+                        playAnimation()
+                    }
+                }
+                imgLottieHeart.setOnClickListener {
+                    imgLottieHeart.apply {
+                        animate().scaleX(0F).scaleY(0F).withEndAction {
+                            isInvisible = true
+                        }
+                    }
+                }
+            }
+        }
+
+
+        private fun getVideoSource(): PlayerView {
+            return PlayerView(context).apply {
+                resizeMode = AspectRatioFrameLayout.RESIZE_MODE_FIXED_HEIGHT
+                useController = false
+                setBackgroundColor(context.color(R.color.white))
+                setShutterBackgroundColor(context.color(R.color.white))
+                //LOADING DEFAULT
+                setShowBuffering(PlayerView.SHOW_BUFFERING_WHEN_PLAYING)
+            }
+        }
+
+        private fun getPlayer(playerCallback: Player.Listener): ExoPlayer {
+            val loadControl = DefaultLoadControl.Builder().setAllocator(DefaultAllocator(true, 16))
+                .setBufferDurationsMs(
+                    VideoPlayerConfig.MIN_BUFFER_DURATION,
+                    VideoPlayerConfig.MAX_BUFFER_DURATION,
+                    VideoPlayerConfig.MIN_PLAYBACK_START_BUFFER,
+                    VideoPlayerConfig.MIN_PLAYBACK_RESUME_BUFFER
+                ).setTargetBufferBytes(-1).setPrioritizeTimeOverSizeThresholds(true).build()
+            return ExoPlayer.Builder(context).setLoadControl(loadControl)
+                .setMediaSourceFactory(DefaultMediaSourceFactory(cacheFactory)).build().apply {
+                    playWhenReady = true
+                    repeatMode = Player.REPEAT_MODE_ONE
+                    addListener(playerCallback)
+                }
+        }
+    }
+
+    private fun ExoPlayer.playVideo(url: String) {
+        val mediaSource: MediaSource =
+            ProgressiveMediaSource.Factory(cacheFactory).createMediaSource(MediaItem.fromUri(url))
+        setMediaSource(mediaSource, true)
+        prepare()
+    }
+
+    object VideoPlayerConfig {
+        //Minimum Video you want to buffer while Playing
+        const val MIN_BUFFER_DURATION = 2000
+
+        //Max Video you want to buffer during PlayBack
+        const val MAX_BUFFER_DURATION = 5000
+
+        //Min Video you want to buffer before start Playing it
+        const val MIN_PLAYBACK_START_BUFFER = 1500
+
+        //Min video You want to buffer when user resumes video
+        const val MIN_PLAYBACK_RESUME_BUFFER = 2000
     }
 }
