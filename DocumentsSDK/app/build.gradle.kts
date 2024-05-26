2671b82046665532e4c3bc013e0bec9e1d5ee501
huyhieu127 <id.huyhieu@gmail.com>
2024-05-27 03:09:59 +0700

    fix build show activity

--- DocumentsSDK/app/build.gradle.kts
+++ DocumentsSDK/app/build.gradle.kts
@@ -1,6 +1,7 @@
 import org.jetbrains.kotlin.gradle.plugin.mpp.pm20.util.archivesName
 
 plugins {
+    //id("com.android.application")
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
 }
